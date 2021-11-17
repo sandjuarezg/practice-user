95e5ef4dde769fb11479fea1c14118ef44cee90f
sandjuarezg <juarez.garcia.sandra.f@gmail.com>
2021-11-16 23:43:49 -0600

    add user dir

---
+++ user/user.go
@@ -0,0 +1,35 @@
+package user
+
+type User struct {
+	User string
+	Pass string
+	Post []string
+}
+
+func AddUser() {
+
+}
+
+func LogIn() {
+
+}
+
+func AddPost() {
+
+}
+
+func EditPost() {
+
+}
+
+func DeletePost() {
+
+}
+
+func ShowAllPosts() {
+
+}
+
+func ShowUserPost() {
+
+}
